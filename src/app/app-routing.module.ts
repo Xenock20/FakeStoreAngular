9eaa0311dd5ad4c08ccfccc07d0f06cfa09b66e0
Xenock20 <juliosaboredo@gmail.com>
2022-12-04 23:58:19 -0300

    Add navbar, components pages and routinglinks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { NotFoundComponent } from './components/page/not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/page/home/home.component';
+import { LoginComponent } from './components/page/login/login.component';
+import { RegisterComponent } from './components/page/register/register.component';
+import { CartComponent } from './components/page/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{path: '', redirectTo: 'home', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'cart', component: CartComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule  } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/page/home/home.component';
+import { LoginComponent } from './components/page/login/login.component';
+import { RegisterComponent } from './components/page/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotFoundComponent } from './components/page/not-found/not-found.component';
+import { CartComponent } from './components/page/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
